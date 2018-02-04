24a4a454e995a17bac15f24294f993891375d5f3
Luca Guidi <me@lucaguidi.com>
2018-02-04 13:16:13 +0100

    Use Hanami.root as string

--- lib/hanami/webconsole/plugin.rb
+++ lib/hanami/webconsole/plugin.rb
@@ -5,5 +5,5 @@ require "binding_of_caller"
 
 Hanami.plugin do
   middleware.use BetterErrors::Middleware
-  BetterErrors.application_root = Hanami.root
+  BetterErrors.application_root = Hanami.root.to_s
 end
